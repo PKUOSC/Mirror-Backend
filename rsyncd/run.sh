75580c12d08604dd283de15dc78769166f765b30
Zhenxin <fuzhenxin95@gmail.com>
2021-03-20 20:44:40 +0800

    Add rsync server configuration

--- rsyncd/run.sh
+++ rsyncd/run.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
-sudo rsync --config /data/backend/rsyncd/rsyncd.conf --daemon 
+files=`ls /data/repos`
+cf='/etc/rsyncd.conf'
+
+cat rsyncd.conf.raw > $cf
+
+for i in $files;
+do
+    if [[ "$i" != "pypi" ]];
+    then
+        echo "[$i]" >> $cf
+	echo comment = Peking University Mirror for $i >> $cf
+	echo path = /data/repos/$i >> $cf
+	echo "" >> $cf
+    fi;
+done;
+
+kill `cat /var/run/rsyncd.pid`
+rsync --daemon
+
